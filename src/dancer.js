14938ef51c6f047133fe4da1be2c2dc5b914b7ac
Hack Reactor Student <student@hackreactor.com>
2017-04-03 20:45:02 -0700

    Add line up feature to Dance Party

--- src/dancer.js
+++ src/dancer.js
@@ -5,6 +5,7 @@ var Dancer = function(top, left, timeBetweenSteps) {
   this.timeBetweenSteps = timeBetweenSteps;
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
+  this.setPosition(top, left);
   this.step();
 };
 
@@ -24,6 +25,21 @@ Dancer.prototype.setPosition = function(top, left) {
   };
   this.$node.css(styleSettings);
 };
+
+Dancer.prototype.lineUp = function(top, left) {
+  var styleSettings = {
+    top: top,
+    left: left
+  };
+  //$('.dancer').animate(styleSettings);
+  this.setPosition(top, left);
+}
+
+/*
+Dancer.prototype.circleTrain = function(top, left) {
+  this.setPosition(top, left);
+}
+*/
 
 // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
 // this one sets the position to some random default point within the body
--- src/init.js
+++ src/init.js
@@ -22,12 +22,57 @@ $(document).ready(function() {
 
     // make a dancer with a random position
 
-    var dancer = dancerMakerFunction(
+    var dancer = new dancerMakerFunction(
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
       Math.random() * 1000
     );
     $('body').append(dancer.$node);
+    
+    window.dancers.push(dancer);
   });
+
+  $('.lineUpDancers').on('click', function(event) {
+
+    //var lineUpFunctionName = $(this).data('line-up-function-name');
+
+    //var lineUpFunction = window[lineUpFunctionName];
+
+    var verticalPos = $('body').height() / 2;
+    var horizontalPos = $('body').width() / window.dancers.length;
+    var left = 15;
+    window.dancers.forEach(function(item) {
+      item.lineUp(verticalPos, left += horizontalPos);
+    });
+  });
+
+  $('.circleTrain').on('click', function(event) {
+
+    //var circleTrainFunctionName = $(this).data('circle-train-function-name');
+
+    //var circleTrainFunction = window[circleTrainFunctionName];
+
+    var vp = $('body').height() / 2;
+    var hp = $('body').width() / 2;
+
+    var sqrTwo = Math.sqrt(2) / 2;
+    var sqrThr = Math.sqrt(3) / 2;
+    var half = 0.5;
+
+
+    var pos = [[hp, vp],
+               [0, -vp],
+               [-hp, 0],
+               [0, vp]];
+
+    //for(var i = 0; i < 4; i++) {
+      window.dancers[0].lineUp(pos[0][0], pos[0][1]);
+     // window.dancers[1].lineUp(pos[i][0], pos[i][1]);
+      //window.dancers[2].lineUp(pos[i][0], pos[i][1]);
+      //window.dancers[3].lineUp(pos[i][0], pos[i][1]);
+    //}
+
+  });
+
 });
 
